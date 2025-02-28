aa35e2231e25ef7475f9d90574dec2611c8cec81
AntoinePerrin25 <antoine.perrin1@utbm.fr>
2025-02-28 16:51:33 +0100

    Add tests.c, test compilation and execution support

--- nob.c
+++ nob.c
@@ -8,6 +8,36 @@ int main(int argc, char **argv)
     // Création des dossiers nécessaires
     nob_mkdir_if_not_exists("build");
     nob_mkdir_if_not_exists("src");
+    nob_mkdir_if_not_exists("tests");
+
+    // tests ?
+
+    if(strcmp(argv[1], "tests") == 0)
+    {
+        // Compilation des tests
+        {
+            Nob_Cmd cmd = {0};
+            nob_cmd_append(&cmd, "gcc");
+            nob_cmd_append(&cmd, "-Wall", "-Wextra", "-O3");
+            nob_cmd_append(&cmd, "-I./include");
+            nob_cmd_append(&cmd, "./tests/tests.c");
+            nob_cmd_append(&cmd, "-o", "./tests/tests");
+            nob_cmd_append(&cmd, "-L./lib");
+            nob_cmd_append(&cmd, "-lraylib", "-lenet", "-lopengl32", "-lgdi32", "-lwinmm", "-lws2_32");
+            if (!nob_cmd_run_sync(cmd)) return 1;
+        }
+
+        // Exécution des tests
+        {
+            Nob_Cmd cmd = {0};
+            nob_cmd_append(&cmd, "./tests/tests");
+            nob_cmd_run_async(cmd);
+        }
+
+        return 0;
+    }
+
+
 
     // Compilation du client
     {
@@ -33,6 +63,20 @@ int main(int argc, char **argv)
         nob_cmd_append(&cmd, "-L./lib");
         nob_cmd_append(&cmd, "-lenet", "-lws2_32");
         if (!nob_cmd_run_sync(cmd)) return 1;
+    }
+
+    // Lancement du serveur
+    {
+        Nob_Cmd cmd = {0};
+        nob_cmd_append(&cmd, "./build/server");
+        nob_cmd_run_async(cmd);
+    }
+
+    // Lancement du client
+    {
+        Nob_Cmd cmd = {0};
+        nob_cmd_append(&cmd, "./build/game");
+        nob_cmd_run_async(cmd);
     }
 
     return 0;
